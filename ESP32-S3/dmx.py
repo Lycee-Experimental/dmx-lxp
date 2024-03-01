4d21ca080bd7cf77d609c119ad149c1da081051e
Davy <davy@ik.me>
2024-03-01 09:32:37 +0100

    Update dmx.py

--- ESP32-S3/dmx.py
+++ ESP32-S3/dmx.py
@@ -1,6 +1,5 @@
 """SPI based CircuitPython DMX transmitter"""
 import busio
-
 import asyncio
 
 BREAK = b'\x00\x00\x00' # 3 x 8 x 4µS = 96µS
@@ -11,7 +10,7 @@ SLOT_0 = '00000000011' # 1 bit start(0), 8 bits (0), 2 stop (1)
 class universe():
     def __init__(self, sck, mosi):
         """DMX SPI initialisation"""
-        self.spi = busio.SPI(sck, MOSI=mosi)
+        self.spi = busio.SPI(sck, mosi)
         self.data = b'\x00'*512
         while not self.spi.try_lock():
             pass
@@ -19,23 +18,25 @@ class universe():
         asyncio.create_task(self.send_dmx())
 
     def byte_to_string(self, byte):
-        """Convert a byte to string with corresponding digits"""
+        """Convert a byte to a string of 8 digits"""
         string = bin(byte)[2:]
         while len(string) < 8:
             string = '0' + string
         return string
 
     def change_data(self, data):
-        """Construct the DMX signal from chanel bytes and change the message"""
-        data_bytes_start_stop = ''.join(['0' + self.byte_to_string(byte) + '11' for byte in data])
-        result = MAB + SLOT_0 + data_bytes_start_stop
-        result += '1' * ((8 - len(result) % 8) % 8)  # Ajout de "1" pour obtenir un multiple de 8
-        # Convert to bytes
+        """Set the DMX databyte signal from channels values"""
+        # MAB+SLOT_0+concatenate channels adding starts (0) and stop (11)
+        result = MAB + SLOT_0 + ''.join(['0' + self.byte_to_string(byte) + '11' for byte in data]) 
+        # Add 1 to the end to obtain a multiple of 8 digits
+        result += '1' * ((8 - len(result) % 8) % 8)  
+        # Convert string of digits to bytes
         result_bytes = int(result, 2).to_bytes(len(result) // 8, 'big')
-        self.data = BREAK + result_bytes + b'\xff'
+        # Set the new DMX databyte to be sent
+        self.data = BREAK + result_bytes
 
     async def send_dmx(self):
-        """Send the DMX message every 10ms"""
+        """Send the DMX message every 100ms"""
         while True:
             self.spi.write(self.data)
-            await asyncio.sleep(0.01)
+            await asyncio.sleep(0.1)
